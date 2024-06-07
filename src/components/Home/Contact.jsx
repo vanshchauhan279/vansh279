2d08b0a08134aced315ffb107ea68ba8fd24b036
ADITYA TYAGI <110872747+adityatyagi-av@users.noreply.github.com>
2024-06-07 15:50:35 +0530

    Update Contact.jsx

--- src/components/Home/Contact.jsx
+++ src/components/Home/Contact.jsx
@@ -6,7 +6,7 @@ import * as Yup from "yup";
 import toast from 'react-hot-toast';
 const Contact = () => {
     const [formSubmitted, setFormSubmitted] = useState(false);
-    const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL,process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY );
+    const supabase = createClient(process.env.SUPABASE_URL,process.env.SUPABASE_ANON_KEY );
     const validationSchema = Yup.object({
         name: Yup.string()
         .max(25, 'Must be 25 characters or less')
