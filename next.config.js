c6a8aa7c601bb66758dfef0abc0043ab2cc1f9c2
ADITYA TYAGI <110872747+adityatyagi-av@users.noreply.github.com>
2024-06-07 16:02:57 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-env: {  SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
-        SUPABASE_URL:process.env.SUPABASE_URL  }
+const nextConfig = {env: {  SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+        SUPABASE_URL:process.env.SUPABASE_URL  }}
+
 module.exports = nextConfig
